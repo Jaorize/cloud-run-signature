55c59068539759cbb5b0ee740ed18c2504429aed
Jaorize <pascal.joris42@gmail.com>
2024-10-23 23:14:42 +0200

    impression image ok

--- main.py
+++ main.py
@@ -81,11 +81,8 @@ def amazon_search():
                     "title": item.item_info.title.display_value,
                     "url": item.detail_page_url,
                     "price": item.offers.listings[0].price.display_amount if item.offers and item.offers.listings else 'N/A',
-                    "condition": item.offers.listings[0].condition.value if item.offers and item.offers.listings and hasattr(item.offers.listings[0], 'condition') else 'N/A',
-                    "star_rating": item.item_info.customer_reviews.star_rating if hasattr(item.item_info, 'customer_reviews') and item.item_info.customer_reviews.star_rating else 'N/A',
                     "primary_image": item.images.primary.large.url if hasattr(item, 'images') and hasattr(item.images, 'primary') and hasattr(item.images.primary, 'large') else 'N/A',
-                    "total_reviews": item.item_info.customer_reviews.total_review_count if hasattr(item.item_info, 'customer_reviews') and hasattr(item.item_info.customer_reviews, 'total_review_count') else 'N/A',
-                    "availability_type": item.offers.listings[0].availability.availability_type if item.offers and item.offers.listings and hasattr(item.offers.listings[0], 'availability') and hasattr(item.offers.listings[0].availability, 'availability_type') else 'N/A'
+
 
                 }
                 for item in response.search_result.items
