184c45d4c91f34b86b458bb134692e30de76fd52
Jaorize <pascal.joris42@gmail.com>
2024-10-29 22:19:00 +0100

    modification ASIN

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def amazon_search():
                             0].price.display_amount if item.offers and item.offers.listings else 'N/A',
                         "primary_image": item.images.primary.large.url if hasattr(item, 'images') and hasattr(
                             item.images, 'primary') and hasattr(item.images.primary, 'large') else 'N/A',
-                        "ASIN":item.asin
+                        "ASIN": item.asin,
                         "prime_eligible": any(
                             listing.delivery_info.is_prime_eligible
                             for listing in item.offers.listings
